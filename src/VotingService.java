b6c085741afe6d5bf5d6d74f7acce7dd61fa5508
Gregorius Avip <gregorius.avip@gmail.com>
2023-10-03 16:50:19 -0700

    Update VotingService.java

--- src/VotingService.java
+++ src/VotingService.java
@@ -1,2 +1,13 @@
+import java.util.*;
 public class VotingService {
+    List<Question> qList = new LinkedList<Question>();
+
+    public void createQuestion(int questionType){
+        if(questionType == 0){
+            qList.add(new SingleQuestion());
+        }
+        else if (questionType == 1) {
+            qList.add(new MultipleQuestion());
+        }
+    }
 }
