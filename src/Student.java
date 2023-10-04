e123cd0dd25b9f8918ed521a7acc69eb357d5837
Gregorius Avip <71865513+gavip1@users.noreply.github.com>
2023-10-03 17:58:46 -0700

    Initialize student object

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,12 @@
 public class Student {
+    private String studentId;
+    public Student(String id){
+        setStudentId(id);
+    }
+    public String getStudentId(){
+        return studentId;
+    }
+    public void setStudentId(String id){
+        this.studentId = id;
+    }
 }
--- src/VotingService.java
+++ src/VotingService.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class VotingService {
     List<Question> qList = new LinkedList<Question>();
-
+    Map<String, Boolean> studentAnswers = new HashMap<String, Boolean>();
     public void createQuestion(int questionType){
         if(questionType == 0){
             qList.add(new SingleQuestion());
@@ -9,5 +9,8 @@ public class VotingService {
         else if (questionType == 1) {
             qList.add(new MultipleQuestion());
         }
+    }
+    public void studentAnswer(){
+
     }
 }
