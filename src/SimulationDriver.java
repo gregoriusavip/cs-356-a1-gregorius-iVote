b2b180a115f34a3841fdcbe23c5c63df6dc5612f
Gregorius Avip <gregorius.avip@gmail.com>
2023-10-05 16:44:50 -0700

    Update SimulationDriver.java

--- src/SimulationDriver.java
+++ src/SimulationDriver.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.List;
@@ -7,13 +8,23 @@ public class SimulationDriver {
         final int RANDOM_STUDENT = random.nextInt(40) + 1;
         final int RANDOM_TOTAL_ANSWERS = random.nextInt(12) + 1;
         List<Student> studentList = new LinkedList<>();
-        for(int i = 0 ; i < RANDOM_STUDENT; i++){
+        List<List<Character>> answerList = new LinkedList<>();
+        for(int i = 0 ; i < RANDOM_STUDENT; i++){   // generate random amount of student
+            List<Character> charAnswer = new LinkedList<>();
             studentList.add(new Student("student" + (i + 1)));
+            int randomAnswer;
+            int maxAnswer = random.nextInt(12) + 1;
+            for(int j = 0; j < maxAnswer; j++){
+                randomAnswer = random.nextInt(RANDOM_TOTAL_ANSWERS) + 1;
+                charAnswer.add((char) ('@' + randomAnswer));
+            }
+            answerList.add(charAnswer);
         }
         VotingService service = new VotingService();
-        service.createQuestion(0, RANDOM_TOTAL_ANSWERS, new int[]{1});
+        service.createQuestion(random.nextInt(2), RANDOM_TOTAL_ANSWERS, new int[]{1});
         for(Student student : studentList){
-            service.answerSingle(student,  'A');
+            service.studentAnswer(student, answerList.getFirst());
+            answerList.removeFirst();
         }
         service.printStatistic();
     }
